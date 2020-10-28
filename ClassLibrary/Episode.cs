5f8a93240b9c7bf1302c0f1da42c4d56a6d40721
JakovTrbara <jakovtrbara2@gmail.com>
2020-10-28 12:51:54 +0100

    Initial commit. Added info.txt

---
+++ ClassLibrary/Episode.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class Episode
+    {
+        private int viewerCount;
+        private double scoreSum;
+        private double maxScore;
+        private double averageRating;
+
+        public double MaxScore
+        {
+            get { return maxScore; }
+            set
+            {
+                if(maxScore<10 && maxScore >= 0)
+                {
+                    maxScore = value;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect Max Score!");
+                }
+            }
+        }
+        
+
+        public Episode()
+        {
+            viewerCount = 0;
+            scoreSum = 0;
+            maxScore = 0;
+        }
+        public Episode(int viewercount, double scoresum,double maxscore) 
+        {
+            viewerCount = viewercount;
+            scoreSum = scoresum;
+            maxScore = maxscore;
+        }
+        public void AddView(double randomScore)
+        {
+            viewerCount++;
+            scoreSum += randomScore;
+            if (randomScore > MaxScore)
+            {
+                MaxScore = randomScore;
+            }
+
+        }
+        public double GetMaxScore()
+        {
+            return MaxScore;
+        }
+        public int GetViewerCount()
+        {
+            return viewerCount;
+        }
+        public double GetAverageScore()
+        {
+            double avgScore = scoreSum / viewerCount;
+            return avgScore;
+        }
+
+        // AddView , GenerateRandomScore, GetMaxScore, GetAverageScore, GetViewerCount
+
+
+
+
+
+
+
+    }
+}
---
+++ DZ1_Project/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using ClassLibrary;
+
+/*Radite sustav koji omogućuje rad: 
+
+s epizodama TV serije 
+s brojem gledatelja
+ukupnom sumom njihovih ocjena 
+i najvećom danom ocjenom iz intervala [0.0-10.0]*/
+namespace DZ1_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+			 double GenerateRandomScore()
+            {
+				double score;
+				Random rand = new Random();
+				score = rand.NextDouble() * 10;
+				score = Math.Round(score, 1);
+				return score;
+            }
+
+			Episode ep1 = new Episode();
+			Episode ep2 = new Episode(10, 64.39, 8.7);
+			int viewers = 10;
+			for (int i = 0; i < viewers; i++)
+			{
+				ep1.AddView(GenerateRandomScore());
+				Console.WriteLine(ep1.GetMaxScore());
+			}
+			if (ep1.GetAverageScore() > ep2.GetAverageScore())
+			{
+				Console.WriteLine($"Viewers: {ep1.GetViewerCount()}");
+			}
+			else
+			{
+				Console.WriteLine($"Viewers: {ep2.GetViewerCount()}");
+			}
+		}
+    }
+}
